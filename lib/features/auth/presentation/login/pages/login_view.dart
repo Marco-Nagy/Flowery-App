36fb6312feb9f66c52b0fd13fc151dc04f625793
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-11-15 22:25:44 +0200

    feat: create remember me widget and text form field

--- lib/features/auth/presentation/login/pages/login_view.dart
+++ lib/features/auth/presentation/login/pages/login_view.dart
@@ -1,6 +1,10 @@
 import 'package:flowery_e_commerce/core/styles/colors/my_colors.dart';
+import 'package:flowery_e_commerce/core/utils/widgets/app_text_form_field.dart';
 import 'package:flowery_e_commerce/core/utils/widgets/base/base_view.dart';
 import 'package:flowery_e_commerce/core/utils/widgets/base/custom_app_bar.dart';
+import 'package:flowery_e_commerce/core/utils/widgets/spacing.dart';
+import 'package:flowery_e_commerce/features/auth/presentation/login/widgets/custom_text_form_field.dart';
+import 'package:flowery_e_commerce/features/auth/presentation/login/widgets/remember_me_widget.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
@@ -8,11 +12,39 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final emailController = TextEditingController();
     return BaseView(
         child: CustomScrollView(
       slivers: [
         SliverToBoxAdapter(
           child: CustomAppBar(appBarTxt: "Login"),
+        ),
+        SliverToBoxAdapter(
+          child: verticalSpacing(30),
+        ),
+        SliverToBoxAdapter(
+            child: CustomTextFormField(
+          controller: emailController,
+          hintText: "Enter your email",
+          labelText: "Email",
+        )),
+        SliverToBoxAdapter(
+          child: verticalSpacing(30),
+        ),
+        SliverToBoxAdapter(
+            child: CustomTextFormField(
+          controller: emailController,
+          hintText: "Enter your password",
+          labelText: "Password",
+        )),
+        SliverToBoxAdapter(
+          child: verticalSpacing(16),
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: RememberMeWidget(onChanged: (value) {}),
+          ),
         )
       ],
     ));
