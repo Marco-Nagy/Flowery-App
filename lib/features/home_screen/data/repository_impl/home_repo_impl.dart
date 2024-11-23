90eed778f0627448b02456bbb32451112b5e4ffe
Somaya Mahmoud <somayamahmoud30@gmail.com>
2024-11-24 01:30:13 +0200

    test: home screen data , domain and presentation layers.

--- lib/features/home_screen/data/repository_impl/home_repo_impl.dart
+++ lib/features/home_screen/data/repository_impl/home_repo_impl.dart
@@ -1,7 +1,6 @@
 import 'package:flowery_e_commerce/core/networking/common/api_result.dart';
 import 'package:flowery_e_commerce/features/home_screen/data/online_data_source/home_online_data_source.dart';
 import 'package:flowery_e_commerce/features/home_screen/domain/entities/best_seller_entity.dart';
-import 'package:flowery_e_commerce/features/home_screen/domain/entities/categories_entity.dart';
 import 'package:flowery_e_commerce/features/home_screen/domain/entities/occasions_entity.dart';
 import 'package:flowery_e_commerce/features/home_screen/domain/repository/home_repo.dart';
 import 'package:injectable/injectable.dart';
@@ -13,11 +12,6 @@ class HomeRepoImpl implements HomeRepository {
   Future<DataResult<List<BestSeller>>> getBestSellers() {
     return homeOnlineDataSource.getBestSellers();
   }
-
-  // @override
-  // Future<DataResult<List<Category>>> getCategories() {
-  //   return homeOnlineDataSource.getCategories();
-  // }
 
   @override
   Future<DataResult<List<Occasions>>> getOccasions() {
